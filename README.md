21034db1b23c7ee3e035b965f88d8e41a1339ca2
halcyra <hhstyyds520@gmail.com>
2025-05-04 03:06:52 +0800

    update README

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # XSync
 An incremental synchronization client and server built on FastCDC and MinIO.
+
+Powered by FastCDC (content defined chunking), retrofit2 (for request on client side), tomcat (server), spring boot, mysql, mybatisplus (persistence framework), redis (for caching), minio (object storage),  jwt (authentication), spring security, guava (speed limit)
--- xsync-server/src/main/java/com/hhst/xsync/XsyncApplication.java
+++ xsync-server/src/main/java/com/hhst/xsync/XsyncApplication.java
@@ -2,11 +2,9 @@ package com.hhst.xsync;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @EnableAsync
-@EnableAspectJAutoProxy
 @SpringBootApplication
 public class XsyncApplication {
 
